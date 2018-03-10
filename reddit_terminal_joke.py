46811a96e90720685ef071438d91cd4755e71c66
Nicole Julian <nicolevjulian@gmail.com>
2018-03-10 18:47:27 -0500

    Enable caching of jokes in local JSON.
    One could make cron job to refill when desired.
    
    do the py2-safe unicode crap only at the end, since JSON gives us unicode anyway

--- reddit_terminal_joke.py
+++ reddit_terminal_joke.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 from random import randint
 
 from reddit_api_helper import RedditAPIHelper
@@ -6,16 +9,24 @@ from reddit_api_helper import RedditAPIHelper
 class RedditJokeFetcher(object):
 
     REQUIRED_UPVOTES = 100  # Be pickier if you want.
+    CACHE_FILENAME = 'reddit_joke_cache.json'
 
     def __init__(self):
         """Instantiate our little friend to make all the HTTP requests."""
         self.api = RedditAPIHelper()
 
+    def _get_cache_path(self):
+        """Return le fancy path to joke cache for file open shenanigans"""
+        __location__ = os.path.realpath(
+            os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+        return os.path.join(__location__, self.CACHE_FILENAME)
+
     def _get_post_item(self, post, item_id):
         """dict diggity diggin'"""
         return post.get('data', {}).get(item_id)
 
-    def get_jokes_posts(self):
+    def _get_jokes_posts(self):
         jokes_data = self.api.get_reddit_response(
             'http://www.reddit.com/r/Jokes.json',
             params={'sort': 'hot', 'limit': self.api.POST_LIMIT},
@@ -25,13 +36,14 @@ class RedditJokeFetcher(object):
 
         return jokes_posts
 
-    def get_decent_joke(self, candidates):
-        """Get a hopefully decent joke from a list of candidate posts.
+    def _get_joke_candidates(self):
+        """Get a stash of hopefully decent jokes.
 
-        We want something hot that is NOT stickied (that announcement stuff
-        that stays on the front page), that actually has a title, actually
-        has text, and has a reasonable number of upvotes.
+        We want hot things that are NOT stickied (that announcement stuff
+        that stays on the front page), that actually have titles, actually
+        have text, and have a reasonable number of upvotes.
         """
+        candidates = self._get_jokes_posts()
         candidates = [
             post for post in candidates
                 if not self._get_post_item(post, 'stickied')
@@ -39,17 +51,46 @@ class RedditJokeFetcher(object):
                 and self._get_post_item(post, 'selftext')
                 and self._get_post_item(post, 'ups') >= self.REQUIRED_UPVOTES
         ]
-        # Let's try to change things up..
-        decent = candidates[randint(0, len(candidates) - 1)]
+        # I am genuinely sorry for how ugly this is...
+        candidates = [
+            {'title': post['data']['title'], 'text': post['data']['selftext']}
+                for post in candidates
+        ]
+        return candidates
 
-        return (decent['data']['title'], decent['data']['selftext'])
+    def _get_cached_jokes(self):
+        """Return jokes from cache if they exist there; None otherwise."""
+        with open(self._get_cache_path(), 'r') as cache_file:
+            try:
+                jokes = json.loads(cache_file.read())
+            except ValueError:  # mmm...l'empty?
+                return None
+
+            return jokes
+
+    def _get_jokes_and_fill_cache(self):
+        """Get fresh jokes from Reddit, write to cache while at it, and return them."""
+        candidates = self._get_joke_candidates()
+        with open(self._get_cache_path(), 'w') as cache_file:
+            json.dump(candidates, cache_file)
+
+        return candidates
+
+    def get_decent_joke(self):
+        """Get cached joke, or refill cache and use one from that process."""
+        jokes = self._get_cached_jokes()
+        if not jokes:
+            jokes = self._get_jokes_and_fill_cache()
+
+        # Let's try to change things up..
+        return jokes[randint(0, len(jokes) - 1)]
 
 
 def main():
     fetcher = RedditJokeFetcher()
-    candidates = fetcher.get_jokes_posts()
-    title, joketext = fetcher.get_decent_joke(candidates)
-    print('{}\n\n{}\n'.format(title.encode('utf-8'), joketext.encode('utf-8')))
+    joke = fetcher.get_decent_joke()
+    print('{}\n\n{}\n'.format(
+        joke['title'].encode('utf-8'), joke['text'].encode('utf-8')))
 
 
 if __name__ == '__main__':
